9f9b2a2e444021b232c75916fcf3e7dbc0358a50
srivatsan1892 <63724241+srivatsan1892@users.noreply.github.com>
2020-10-01 20:54:50 +0530

    Update ctrllight.py

--- ctrllight.py
+++ ctrllight.py
@@ -4,7 +4,7 @@ import os
 ADAFRUIT_IO_USERNAME =os.getenv('ADAFRUIT_IO_USERNAME')
 ADAFRUIT_IO_KEY =os.getenv('ADAFRUIT_IO_KEY')
 aio = Client('ADAFRUIT_IO_USERNAME','ADAFRUIT_IO_KEY')
-
+TELEGRAM_TOKEN=os.getenv('TELEGRAM_TOKEN')
 
 def cloudval(value):
   feed=aio.feeds('ctrllight')

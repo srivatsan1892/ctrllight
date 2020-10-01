da0581777eff4f4371ece63a276d7e03fc2c3995
srivatsan1892 <63724241+srivatsan1892@users.noreply.github.com>
2020-10-01 21:02:29 +0530

    Update ctrllight.py

--- ctrllight.py
+++ ctrllight.py
@@ -1,31 +1,32 @@
 from Adafruit_IO import Client, Feed,Data
 from telegram.ext import Updater,CommandHandler
 import os
-ADAFRUIT_IO_USERNAME =os.getenv('ADAFRUIT_IO_USERNAME')
-ADAFRUIT_IO_KEY =os.getenv('ADAFRUIT_IO_KEY')
-aio = Client('ADAFRUIT_IO_USERNAME','ADAFRUIT_IO_KEY')
-TELEGRAM_TOKEN=os.getenv('TELEGRAM_TOKEN')
+ADAFRUIT_IO_USERNAME = os.getenv('ADAFRUIT_IO_USERNAME')   # adafruit username and password should be given as 'Config Vars' in the settings of your app on Heroku 
+ADAFRUIT_IO_KEY = os.getenv('ADAFRUIT_IO_KEY')
+aio = Client('ADAFRUIT_IO_USERNAME','ADAFRUIT_IO_KEY') # create instance of REST client
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')  # similar to the adafruit username and password
 
-def cloudval(value):
-  feed=aio.feeds('ctrllight')
-  aio.send_data(feed.key,value)
-def On(bot,update):
-  chat_id=update.message.chat_id
-  bot.send_message(chat_id,"Lights On")
-  bot.send_photo(chat_id, photo='https://images.app.goo.gl/EgmisKBLpz82bHqv6')
-  cloudval(1)
-def Off(bot,update):
+
+def valcloud(value):
+  to_feed = aio.feeds('ctrllight') # put youctrllightr own feed name here
+  aio.send_data(to_feed.key,value)  # append a new value to a feed
+
+# function to switch on light and send value '1' to adafruit
+def On(bot, update):
+  chat_id = update.message.chat_id
+  bot.send_message(chat_id, text="Lights ON")
+  bot.send_photo(chat_id, photo='https://www.securityroundtable.org/wp-content/uploads/2019/03/AdobeStock_261504199-scaled.jpeg')
+  valcloud(1)
+#function to switch off the light and send value '0' to adafruit
+def lightoff(bot, update):
   chat_id = update.message.chat_id
   bot.send_message(chat_id, text="Lights OFF")
-  bot.send_photo(chat_id,photo='https://images.app.goo.gl/BLFdTFJ9TsJ3okD49')
-  cloudval(0)
-  
+  bot.send_photo(chat_id=update.effective_chat.id,photo='https://ak.picdn.net/shutterstock/videos/1027638404/thumb/1.jpg?ip=x480')
+  valcloud(0)
 
-u = Updater('TELEGRAM_TOKEN') 
+u = Updater('TELEGRAM_TOKEN',use_context = True) 
 dp = u.dispatcher
-dp.add_handler(CommandHandler('lighton',On))  
-dp.add_handler(CommandHandler('lightoff',Off))
-
-u.start_polling() 
-u.idle()
- 
+dp.add_handler(CommandHandler('lighton',lighton))  # register a handler
+dp.add_handler(CommandHandler('lightoff',lightoff))
+u.start_polling()  # starts polling updates from Telegram
+u.idle() # blocks until one of the signals are received and stops the updater
